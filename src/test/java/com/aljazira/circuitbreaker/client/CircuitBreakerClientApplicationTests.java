55bb50120902541b516b13a8c4e08ce12c55aa9c
Faisal Ahmed <faisal.dev@gmail.com>
2020-06-25 16:36:21 +0300

    Initial commit

---
+++ src/test/java/com/aljazira/circuitbreaker/client/CircuitBreakerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljazira.circuitbreaker.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
